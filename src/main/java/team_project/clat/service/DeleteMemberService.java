878abd85a042a7666cdf9dd21f97bfdb8fe750c5
akk <rudrhs9309@naver.com>
2024-12-05 17:47:20 +0900

    hotfix : 회원탈퇴 조건 및 예외 처리 추가

--- src/main/java/team_project/clat/service/DeleteMemberService.java
+++ src/main/java/team_project/clat/service/DeleteMemberService.java
@@ -2,8 +2,10 @@ package team_project.clat.service;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ import team_project.clat.repository.TokenRepository;
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class DeleteMemberService {
 
     private final AuthenticationManager authenticationManager;
@@ -29,31 +32,32 @@ public class DeleteMemberService {
 
         //입력한 username(id)가 일치하는지 확인
         if(deleteMemberReqDTO.getUsername().equals(username)){
+
             UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, deleteMemberReqDTO.getPassword(), null);
-            Authentication authenticate = authenticationManager.authenticate(authToken);
+            try {
+                Authentication authenticate = authenticationManager.authenticate(authToken);
+                //id, password 인증
+                if(authenticate.isAuthenticated()){
+                    memberRepository.deleteByUsername(username);
 
-            //id, password 인증
-            if(authenticate.isAuthenticated()){
-                memberRepository.deleteByUsername(username);
+                    boolean isExist = tokenRepository.existsById(username);
+                    if (!isExist) {
+                        throw new RefreshTokenNotFound("Refresh 토큰이 존재하지 않습니다.");
+                    }
+                    //Refresh 토큰 DB에서 제거
+                    tokenRepository.deleteById(username);
 
-                boolean isExist = tokenRepository.existsById(username);
-                if (!isExist) {
+                    //회원 탈퇴 완료
+                    CommonResultResDTO commonResultResDTO = new CommonResultResDTO("200 OK", "회원 탈퇴가 완료되었습니다.");
+                    return new DeleteMemberResDTO(commonResultResDTO, null, null, HttpStatus.OK);
 
-                    //response status code
-                   throw new UnAuthorizationException("아이디, 비밀번호가 일치하지 않습니다.");
+                }else {
+                    //id, password 일치하지 않음
+                    throw new UnAuthorizationException("아이디, 비밀번호가 일치하지 않습니다.");
                 }
-
-                //로그아웃 진행
-                //Refresh 토큰 DB에서 제거
-                tokenRepository.deleteById(username);
-
-            }else {
-                //id, password 일치하지 않음
-                throw new RefreshTokenNotFound("Refresh 토큰이 존재하지 않습니다.");
+            }catch (BadCredentialsException e){
+                throw new UnAuthorizationException("아이디 또는 비밀번호가 일치하지 않습니다.");
             }
-        }
-
-        CommonResultResDTO commonResultResDTO = new CommonResultResDTO("200 OK", "회원 탈퇴가 완료되었습니다.");
-        return new DeleteMemberResDTO(commonResultResDTO, null, null, HttpStatus.OK);
+        }else throw new UnAuthorizationException("아이디, 비밀번호가 일치하지 않습니다.");
     }
 }
