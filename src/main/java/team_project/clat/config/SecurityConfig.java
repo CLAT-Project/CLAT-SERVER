120ae12d0d953bd0c82d7b52d0bbac024679baae
akk <rudrhs9309@naver.com>
2024-12-05 15:19:29 +0900

    feat : 프론트 신규 배포 서버, CORS 설정 등록

--- src/main/java/team_project/clat/config/SecurityConfig.java
+++ src/main/java/team_project/clat/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
                 CorsConfiguration configuration = new CorsConfiguration();
 
-                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://c-lat.site", "https://clat-client.vercel.app", "https://chat-project-dun.vercel.app"));
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://c-lat.site", "https://clat-client.vercel.app", "https://chat-project-one-sandy.vercel.app"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 configuration.setAllowedHeaders(Arrays.asList("Authorization", "access", "Content-Type","Set-Cookie"));
                 configuration.setAllowCredentials(true);
