3a9210713748722acbd4732ad908d6ea1ee6d958
kcsc2217 <k12002@nate.com>
2024-12-16 12:59:45 +0900

    fix: 임시 저장

--- src/main/java/team_project/clat/config/SecurityConfig.java
+++ src/main/java/team_project/clat/config/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
                 .requestMatchers( "/chatRoom").hasRole("PROFESSOR")
                 .requestMatchers("/chatRoom/api/**").hasRole("PROFESSOR")
                 .requestMatchers("/reIssue", "/delete", "/member/findPwd").permitAll()
-                .anyRequest().authenticated());
+                .anyRequest().permitAll());
 
 
         http.addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
