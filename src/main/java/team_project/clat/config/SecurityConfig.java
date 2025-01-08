def3d0280292422e2e566e4f7a0eb2f6066aad31
akk <rudrhs9309@naver.com>
2025-01-09 00:10:53 +0900

    feat : OAuth2 의존성 및 기본 설정 추가

--- src/main/java/team_project/clat/config/SecurityConfig.java
+++ src/main/java/team_project/clat/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -81,6 +82,9 @@ public class SecurityConfig {
 
         //http basic 인증 방식 disable
         http.httpBasic((auth) -> auth.disable());
+
+        //oauth2
+        http.oauth2Login(Customizer.withDefaults());
 
         //경로별 인가 작업
         http.authorizeHttpRequests((auth) -> auth
