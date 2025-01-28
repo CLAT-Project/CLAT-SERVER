3365f3015b480b68de1d9088b99a9a1bac20d948
akk <rudrhs9309@naver.com>
2025-01-28 23:10:04 +0900

    test : test 원복

--- src/main/java/team_project/clat/config/handler/CustomSuccessHandler.java
+++ src/main/java/team_project/clat/config/handler/CustomSuccessHandler.java
@@ -88,7 +88,6 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
             //response.addCookie(createCookie("refresh", refresh));
             response.setHeader(HttpHeaders.SET_COOKIE, createCookie("refresh", refresh).toString());
             response.setStatus(HttpStatus.OK.value());
-            response.sendRedirect("https://clat-project.vercel.app/home");
         }
     }
 
