bb7498fb1f83804758356306474677fcb588e9ec
akk <rudrhs9309@naver.com>
2025-03-03 22:59:26 +0900

    fix : 카카오 코드 수정

--- src/main/java/team_project/clat/oauth2/CustomClientRegistrationRepository.java
+++ src/main/java/team_project/clat/oauth2/CustomClientRegistrationRepository.java
@@ -14,6 +14,8 @@ public class CustomClientRegistrationRepository {
     public ClientRegistrationRepository clientRegistrationRepository(){
         return new InMemoryClientRegistrationRepository(
                 socialClientRegistration.naverClientRegistration(),
-                socialClientRegistration.googleClientRegistration());
+                socialClientRegistration.googleClientRegistration(),
+                socialClientRegistration.kakaoClientRegistration()
+        );
     }
 }
